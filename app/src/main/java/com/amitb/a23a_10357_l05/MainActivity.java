48465e81d5562a868aec657bbda8d8662eab4b12
amitbarel <amitbrl1797@gmail.com>
2022-11-23 14:20:41 +0200

    Initial commit

---
+++ app/src/main/java/com/amitb/a23a_10357_l05/MainActivity.java
@@ -0,0 +1,14 @@
+package com.amitb.a23a_10357_l05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
